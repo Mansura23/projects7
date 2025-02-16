2cb5c82c2968dcfdfcb644acdc8d1413a8d5c0a4
Mansura <badalovamansura40@gmail.com>
2025-02-16 14:39:42 +0400

    project commited

---
+++ src/main/java/turing/edu/az/modul1/lesson1/LoopCoddition.java
@@ -0,0 +1,7 @@
+package turing.edu.az.modul1.lesson1;
+
+public class LoopCoddition {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/turing/edu/az/modul1/lesson2/Datatypes.java
@@ -0,0 +1,7 @@
+package turing.edu.az.modul1.lesson2;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        System.out.println("Salam Dunya");
+    }
+}
